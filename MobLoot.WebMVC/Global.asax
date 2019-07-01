ea3ec7ac91fea73a31ff5c5607d7312da04ab556
Chase00 <chasekuhnmc@gmail.com>
2019-07-01 16:39:25 -0400

    Add project files.

---
+++ MobLoot.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MobLoot.WebMVC.MvcApplication" Language="C#" %>
---
+++ MobLoot.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MobLoot.WebMVC.Startup))]
+namespace MobLoot.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
