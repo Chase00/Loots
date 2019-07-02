f6096ab063a13c2b35bc4eb46a5c3bbbf465ed02
Chase00 <chasekuhnmc@gmail.com>
2019-07-02 15:17:52 -0400

    Added the Monster class

--- MobLoot.Data/IdentityModels.cs
+++ MobLoot.Data/IdentityModels.cs
@@ -32,7 +32,7 @@ namespace MobLoot.Data
             return new ApplicationDbContext();
         }
 
-        public DbSet<Note> Notes { get; set; }
+        public DbSet<Monsters> Monsters { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
---
+++ MobLoot.Data/Monsters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobLoot.Data
+{
+    public class Monsters
+    {
+        [Key]
+        public int MonsterId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "There are too many characters in this field.")]
+        [Display(Name = "Monster: ")]
+        public string MonsterName { get; set; }
+        [Required]
+        [MaxLength(400, ErrorMessage = "There are too many characters in this field.")]
+        [Display(Name = "Description:  ")]
+        public string MonsterDesc { get; set; }
+        [Required]
+        [Range(1, 200, ErrorMessage = "Please choose a number between 1 and 200")]
+        [Display(Name = "Lvl: ")]
+        public short MonsterLevel { get; set; }
+    }
+}
