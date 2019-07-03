14ea452a65d113ad15d90f7f8f13e332d0806bad
Chase00 <chasekuhnmc@gmail.com>
2019-07-03 11:29:56 -0400

    Added MonstersService

---
+++ MobLoot.Services/MonstersService.cs
@@ -0,0 +1,57 @@
+﻿using MobLoot.Data;
+using MobLoot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobLoot.Services
+{
+    public class MonstersService
+    {
+        private readonly Guid _userId;
+        public MonstersService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateMonster(MonstersCreate model)
+        {
+            var entity =
+                new Monsters()
+                {
+                    OwnerId = _userId,
+                    MonsterName = model.MonsterName,
+                    MonsterDesc = model.MonsterDesc,
+                    MonsterLevel = short.Parse(model.MonsterLevel)
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Monsters.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<MonstersListItem> GetMonsters() // Allows us to see the specific notes that belong to a specific user
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx
+                    .Monsters
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                            new MonstersListItem
+                            {
+                                MonsterName = e.MonsterName,
+                                MonsterDesc = e.MonsterDesc,
+                                MonsterLevel = e.MonsterLevel,
+                            }
+                    );
+                return query.ToArray();
+            }
+        }
+    }
+}
